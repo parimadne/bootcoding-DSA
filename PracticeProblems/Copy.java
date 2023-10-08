9ce87ca8b7dc885729989b2e6443858124323c53
parimadne <parimadne04@gmail.com>
2023-10-08 22:22:14 +0530

    Basic DSA

--- PracticeProblems/Copy.java
+++ PracticeProblems/Copy.java
@@ -7,12 +7,12 @@ public class Copy {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] a = new int[n];
-        System.out.println("matrix A:");
+        System.out.println("Array A:");
         for (int i = 0; i < a.length; i++) {
             a[i] = sc.nextInt();
         }
         int[] b = new int[n];
-        System.out.println("matrix B:");
+        System.out.println("Array B:");
         for(int i=0;i<a.length;i++) {
             b[i] = a[i];
             System.out.print(b[i]+" ");
