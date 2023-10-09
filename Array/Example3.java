66f4da31b0e23dc4f915bdb74f3145fd089edaef
parimadne <parimadne04@gmail.com>
2023-10-10 00:09:15 +0530

    Basic DSA

---
+++ Array/Example3.java
@@ -0,0 +1,30 @@
+package Array;
+import java.util.Scanner;
+public class Example3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i = 0;i<a.length;i++){
+            a[i]= sc.nextInt();
+        }
+        int p = sc.nextInt();
+        int indextoremove=p;
+        if(indextoremove>=0 && indextoremove<a.length){
+            int[]dummy = new int[a.length-1];
+            int j =0;
+            for(int i =0;i<a.length;i++){
+                if(i != indextoremove){
+                    dummy[j]=a[i];
+                    j++;
+                }
+            }
+           for(int i =0;i<dummy.length;i++){
+               System.out.print(dummy[i]+" ");
+           }
+        }
+        else {
+            System.out.println("Array Element not found ");
+        }
+    }
+}
