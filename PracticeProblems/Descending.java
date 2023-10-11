8295855341ce3c77244384e91c48ec6ca56eb93a
parimadne <parimadne04@gmail.com>
2023-10-12 00:04:42 +0530

    Basic DSA

---
+++ PracticeProblems/Descending.java
@@ -0,0 +1,30 @@
+package PracticeProblems;
+
+import java.util.Scanner;
+
+public class Descending {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[]a = new int[n];
+        for(int i = 0;i<a.length;i++){
+            a[i]=sc.nextInt();
+        }
+
+        int p= a.length;
+
+        for(int i=0;i<p;i++){
+            for(int j=0;j<p-i-1;j++){
+                if(a[j]<a[j+1]){
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+
+        }
+        for (int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
