e27c631071c82d6aa136b110a6bbf48cd547ea9e
parimadne <parimadne04@gmail.com>
2023-10-25 01:23:40 +0530

    Basic DSA

---
+++ PracticeProblems/ReverseArray.java
@@ -0,0 +1,29 @@
+package PracticeProblems;
+import java.util.Scanner;
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Reverse Array:");
+        int[] arr = reverse(a);
+        for(int i=0;i<a.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static int[] reverse(int[]arr){
+        int i = 0;
+        int j = arr.length-1;
+        while(i<=j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return arr;
+}
+}
