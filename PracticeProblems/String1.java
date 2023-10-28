b52b41a6fb47f68a579e15b5bc28521d4e2b2f58
parimadne <parimadne04@gmail.com>
2023-10-28 06:14:19 +0530

    Basic DSA

---
+++ PracticeProblems/String1.java
@@ -0,0 +1,13 @@
+package PracticeProblems;
+
+public class String1 {
+    public static void main(String[] args) {
+        String name="Parimita";
+        String s0= name.toLowerCase();
+        System.out.println(s0);
+        String s1 = name.toUpperCase();
+        System.out.println(s1);
+        String s2 = name.replace('t','T');
+        System.out.println(s2);
+    }
+}
