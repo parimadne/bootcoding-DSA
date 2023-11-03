51320a7b186f5c1c26679013abdc6b0f63196d79
parimadne <parimadne04@gmail.com>
2023-11-03 23:42:43 +0530

    Basic DSA

---
+++ Assignment/Question2.java
@@ -0,0 +1,11 @@
+package Assignment;
+import java.util.Scanner;
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("String: ");
+        String a=sc.next();
+        String p = a.toUpperCase();
+        System.out.println(p);
+    }
+}
