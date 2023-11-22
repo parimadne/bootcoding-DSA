d9aeaa25b4c702187eced472b8a220cc05027648
parimadne <parimadne04@gmail.com>
2023-11-22 08:33:07 +0530

    Basic DSA

---
+++ Assignment/Question7.java
@@ -0,0 +1,10 @@
+package Assignment;
+import java.util.Scanner;
+public class Question7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String str1 = a.trim();
+        System.out.println(str1);
+    }
+}
